22926a847f043a60adb4ca6b84f782af25311451
Yaroslav.K <fokrivosheev@mail.ru>
2026-06-30 23:02:27 +0500

    Refactoring config.go Implementation of logging slog

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"errors"
-	"flag"
 	"fmt"
 	"os"
 	"time"
@@ -12,9 +11,9 @@ import (
 
 // Config содержит все настройки приложения
 type Config struct {
-	Env         string     `yaml:"env" env-required:"true"`
-	StoragePath string     `yaml:"storage_path" env-required:"true"`
-	HTTPServer  HTTPServer `yaml:"http_server"`
+	Env          string     `yaml:"env" env-required:"true"`
+	StoragePath  string     `yaml:"storage_path" env-required:"true"`
+	ServerConfig HTTPServer `yaml:"http_server"`
 }
 
 // HTTPServer содержит настройки для запуска HTTP-сервера
@@ -25,54 +24,23 @@ type HTTPServer struct {
 }
 
 // MustLoad загружает конфигурацию и паникует в случае ошибки.
-func MustLoad() *Config {
-	cfg, err := Load()
-	if err != nil {
-		panic(fmt.Sprintf("critical error loading config: %v", err))
-	}
-
-	return cfg
-}
-
-func Load() (*Config, error) {
-	configPath := fetchConfigPath()
+func MustLoad(configPath string) *Config {
 	if configPath == "" {
-		return nil, fmt.Errorf("config path is empty(set GONFIG_PATH or --config flag)")
+		panic("config path is empty")
 	}
 
-	// Проверяем, существует ли файл физически
 	_, err := os.Stat(configPath)
+
 	if errors.Is(err, os.ErrNotExist) {
-		return nil, fmt.Errorf("config file missing: %s", configPath)
-	} else if err != nil {
-		return nil, fmt.Errorf("error checking config file: %w", err)
+		panic("config file does not exist: " + configPath)
 	}
 
 	var cfg Config
 
+	// Читаем конфиг и оборачиваем ошибку, если что-то пошло не так
 	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
-		return nil, fmt.Errorf("failed to read config file: %w", err)
+		panic(fmt.Sprintf("failed to read config: %v", err))
 	}
 
-	return &cfg, err
-}
-
-// fetchConfigPath выбирает откуда взять путь к конфигу
-// Приоритет: флаг командной строки -> переменная окружения
-func fetchConfigPath() string {
-	var res string
-
-	// flag.StringVar позволяет передавать --config="path/to/config.yaml"
-	// Проверка flag.Parsed() нужна, чтобы не вызывать Parse повторно в тестах
-	if !flag.Parsed() {
-		flag.StringVar(&res, "config", "", "path to configuration file")
-		flag.Parse()
-	}
-
-	// Если флаг пустой, смотрим в окружение
-	if res == "" {
-		res = os.Getenv("GONFIG_PATH")
-	}
-
-	return res
+	return &cfg
 }
