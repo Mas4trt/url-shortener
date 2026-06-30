4b25ebf4cc95e6f69f296bd06a9c8c0729d5a371
Yaroslav.K <fokrivosheev@mail.ru>
2026-07-01 02:13:25 +0500

    fix: add storage directory creation and update SQLite integration

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,6 +5,8 @@ import (
 	"log/slog"
 	"os"
 	"url-shortener/internal/config"
+	sl "url-shortener/internal/lib/logger/slog"
+	"url-shortener/internal/storage/sqlite"
 )
 
 const (
@@ -21,12 +23,18 @@ func main() {
 
 	log := setupLogger(cfg.Env)
 
+	//TODO: init logger: slog
 	log.Info("starting url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
 
-	//TODO: init logger: slog
-
 	//TODO: init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 
 	//TODO: init router: chi, chi render
 
