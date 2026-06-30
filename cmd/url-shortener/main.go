a97f000583b3a20a7589e058615e4a4e29e56b59
Yaroslav.K <fokrivosheev@mail.ru>
2026-06-30 23:04:25 +0500

    Create func setupLogger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	//TODO: init config: cleanenv
-	cfg := config.MustLoad()
+	configPath := fetchConfigPath()
 
-	fmt.Println(cfg)
+	cfg := config.MustLoad(configPath)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	//TODO: init logger: slog
 
@@ -18,4 +31,49 @@ func main() {
 	//TODO: init router: chi, chi render
 
 	//TODO: run server
+}
+
+// fetchConfigPath выбирает откуда взять путь к конфигу
+// Приоритет: флаг командной строки -> переменная окружения
+func fetchConfigPath() string {
+	var res string
+
+	// flag.StringVar позволяет передавать --config="path/to/config.yaml"
+	// Проверка flag.Parsed() нужна, чтобы не вызывать Parse повторно в тестах
+	if !flag.Parsed() {
+		flag.StringVar(&res, "config", "", "path to configuration file")
+		flag.Parse()
+	}
+
+	// Если флаг пустой, смотрим в окружение
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
