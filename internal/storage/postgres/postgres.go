57171e804dcb9aa72edfa7fc325923b2fd609222
Yaroslav.K <fokrivosheev@mail.ru>
2026-07-03 16:30:03 +0500

    feat: add method delete. fix: op rename sglite

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -17,15 +17,15 @@ type Storage struct {
 
 // New инициализирует базу данных и создает необходимые таблицы
 func New(ctx context.Context, connString string) (*Storage, error) {
-	const op = "storage.sqlite.New"
+	const op = "storage.postgres.New"
 
 	pool, err := pgxpool.New(ctx, connString)
 	if err != nil {
-		return nil, fmt.Errorf("%s : %w", op, err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	if err := pool.Ping(ctx); err != nil {
-		return nil, fmt.Errorf("%s :ping failed: %w", op, err)
+		return nil, fmt.Errorf("%s: ping failed: %w", op, err)
 	}
 
 	return &Storage{pool: pool}, nil
@@ -38,32 +38,40 @@ func (s *Storage) Close() {
 
 // SaveURL сохраняет URL и его алиас в БД
 func (s *Storage) SaveURL(ctx context.Context, urlToSave string, alias string) (int64, error) {
-	const op = "storage.sqlite.SaveURL"
+	const op = "storage.postgres.SaveURL"
 
 	var id int64
-	query := `INSERT INTO urlshortener.url (url, alias) VALUES ($1, $2) RETURNING id`
+	const insertURLQuery = `
+		INSERT INTO urlshortener.url (url, alias) 
+		VALUES ($1, $2) 
+		RETURNING id
+	`
 
-	err := s.pool.QueryRow(ctx, query, urlToSave, alias).Scan(&id)
+	err := s.pool.QueryRow(ctx, insertURLQuery, urlToSave, alias).Scan(&id)
 	if err != nil {
 		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
-			return 0, fmt.Errorf("%s: %w", op, domain.ErrURLExist)
+		if errors.As(err, &pgErr) {
+			switch {
+			case pgErr.Code == "23505" && pgErr.ConstraintName == "url_alias_key":
+				return 0, fmt.Errorf("%s: %w", op, domain.ErrURLExist)
+			}
 		}
-		return 0, fmt.Errorf("%s : %w", op, err)
+		return 0, fmt.Errorf("%s: %w", op, err)
 	}
 
 	return id, nil
 }
 
 func (s *Storage) GetURL(ctx context.Context, alias string) (string, error) {
-	const op = "storage.sqlite.GetURL"
+	const op = "storage.postgres.GetURL"
 
 	var urlToGet string
-	query := `SELECT url FROM urlshortener.url WHERE alias = $1`
+	const selectURLQuery = `
+		SELECT url FROM urlshortener.url 
+		WHERE alias = $1
+	`
 
-	// QueryRowContext выполняет запрос, ожидая ровно одну строку
-	// Сразу же вызываем Scan, чтобы записать результат в urlToGet
-	err := s.pool.QueryRow(ctx, query, alias).Scan(&urlToGet)
+	err := s.pool.QueryRow(ctx, selectURLQuery, alias).Scan(&urlToGet)
 	if err != nil {
 		// Проверяем, вернулась ли ошибка из-за того, что запись не найдена
 		if errors.Is(err, pgx.ErrNoRows) {
@@ -75,24 +83,23 @@ func (s *Storage) GetURL(ctx context.Context, alias string) (string, error) {
 	return urlToGet, nil
 }
 
-// func (s *Storage) DeleteURL(ctx context.Context, alias string) error {
-// 	const op = "storage.sqlite.DeleteURL"
+func (s *Storage) DeleteURL(ctx context.Context, alias string) error {
+	const op = "storage.postgres.DeleteURL"
 
-// 	res, err := s.db.ExecContext(ctx, `DELETE FROM url WHERE alias = ?`, alias)
-// 	if err != nil {
-// 		return fmt.Errorf("%s: %w", op, err)
-// 	}
+	const deleteURLQuery = `
+		DELETE FROM url 
+		WHERE alias = $1
+	`
 
-// 	// Получаем количество удаленных строк
-// 	rowsAffected, err := res.RowsAffected()
-// 	if err != nil {
-// 		return fmt.Errorf("%s: failed to get rows affected: %w", op, err)
-// 	}
+	tag, err := s.pool.Exec(ctx, deleteURLQuery, alias)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
 
-// 	// Если ни одна строка не затронута, значит алиас не найден
-// 	if rowsAffected == 0 {
-// 		return fmt.Errorf("%s : %w", op, storage.ErrURLNotFound)
-// 	}
+	// Если ни одна строка не затронута, значит алиас не найден
+	if tag.RowsAffected() != 1 {
+		return fmt.Errorf("%s: %w", op, domain.ErrURLNotFound)
+	}
 
-// 	return nil
-// }
+	return nil
+}
