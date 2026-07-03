0a96e99d4518e9d780f45cb28926b34ea32e4c42
Yaroslav.K <fokrivosheev@mail.ru>
2026-07-03 22:59:34 +0500

    feat: add new error

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrURLNotFound = errors.New("url not found")
 	ErrURLExist    = errors.New("url already exist")
 	ErrEmptyAlias  = errors.New("empty alias")
+	ErrInvalidURL  = errors.New("empty url")
 )
