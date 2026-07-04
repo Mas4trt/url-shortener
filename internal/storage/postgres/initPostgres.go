7232faab3001fec51346d523d149dd81b3806288
Yaroslav <fokrivosheev@mail.ru>
2026-07-04 22:59:41 +0500

    feat: add a function that cleans up the db

--- internal/storage/postgres/initPostgres.go
+++ internal/storage/postgres/initPostgres.go
@@ -6,25 +6,35 @@ import (
 	"log/slog"
 	"time"
 	"url-shortener/pkg/logger/sl"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func InitPostgres(log *slog.Logger, dsn string) (*PostgresRepo, error) {
+func InitPostgres(log *slog.Logger, dsn string) (*PostgresRepo, func(), error) {
 	const op = "postgres.InitPosttgres"
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	db, err := New(ctx, dsn)
+	pool, err := pgxpool.New(ctx, dsn)
 	if err != nil {
-		log.Error("failed to connect postgres", sl.Err(err))
-		return nil, fmt.Errorf("%s: %w", op, err)
+		log.Error("failed to configure postgres pool", sl.Err(err))
+		return nil, nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	if err := db.pool.Ping(ctx); err != nil {
-		db.pool.Close()
-		log.Error("failed to connect postgres", sl.Err(err))
-		return nil, fmt.Errorf("%s: %w", op, err)
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+		log.Error("failed to connect to postgres", sl.Err(err))
+		return nil, nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	return db, nil
+	repo := &PostgresRepo{pool: pool}
+
+	cleanup := func() {
+		log.Info("closing postgres connection pool")
+		repo.pool.Close()
+	}
+
+	log.Info("postgres connection established")
+	return repo, cleanup, nil
 }
--- internal/storage/redis/initRedis.go
+++ internal/storage/redis/initRedis.go
@@ -10,22 +10,30 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-func InitRedis(log *slog.Logger, addr string) (*redis.Client, error) {
+func InitRedis(log *slog.Logger, addr string) (*redis.Client, func(), error) {
 	const op = "redis.InitRedis"
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	rdb := redis.NewClient(&redis.Options{
-		Addr: addr,
+		Addr:         addr,
+		PoolSize:     10,
+		MinIdleConns: 2,
 	})
 
 	if err := rdb.Ping(ctx).Err(); err != nil {
 		log.Error("redis not available", sl.Err(err))
-		return nil, fmt.Errorf("%s: %w", op, err)
+		return nil, nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	cleanup := func() {
+		log.Info("closing redis connection")
+		if err := rdb.Close(); err != nil {
+			log.Error("failed to close redis connection", sl.Err(err))
+		}
 	}
 
 	log.Info("redis initialized")
-
-	return rdb, nil
+	return rdb, cleanup, nil
 }
